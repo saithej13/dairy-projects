579fdb76abf15c26468505fc04bead744f9852e7
sai <saithej.13@gmail.com>
2023-06-16 00:05:27 +0530

    15JUNE2023

--- src/Components/api.js
+++ src/Components/api.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { userAction } from '../Actions/userAction';
 
 export const API_URL = 'http://192.168.1.8:8080/api';
 
@@ -9,37 +11,63 @@ const api = axios.create({
       'Content-Type': 'application/json',
     },
   });
-  const accessToken = localStorage.getItem('accessToken');
-  if (accessToken) {
-    api.defaults.headers.common['Authorization'] = accessToken;
-  }
+  // const accessToken = localStorage.getItem('accessToken');
+  // if (accessToken) {
+  //   api.defaults.headers.common['Authorization'] = accessToken;
+  // }
   /*export const setAccessToken = (accessToken) => {
     console.log('its logging in setAccessToken of Api')
     console.log('Access Token:', accessToken);
     api.defaults.headers.common['Authorization'] = accessToken;
   };*/
+
+  api.interceptors.request.use(
+    (config) => {
+      const accessToken = localStorage.getItem('accessToken');
+      if (accessToken) {
+        config.headers['Authorization'] = accessToken;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+  
+  // Create a dispatch function outside the interceptor
+dispath = useDispatch();
+// Add a response interceptor
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Handle 401 Unauthorized error
+      // Clear token, dispatch logout action, redirect, and show error message to the user
+      localStorage.removeItem('accessToken');
+      window.location.href = '/'; // Redirect to login page
+      alert('Session Expired , Please Login');
+      dispath
+    }
+    return Promise.reject(error);
+  }
+);
+  
   
   export const clearAccessToken = () => {
     delete api.defaults.headers.common['Authorization'];
   };
   
   export const get = (url) => {
-    const token = localStorage.getItem('accessToken');
-    if (token) {
-      api.defaults.headers.common['Authorization'] = token;
-    } else {
-      delete api.defaults.headers.common['Authorization'];
-    }
     return api.get(url);
   };
   
   export const post = (url, data) => {
-    const token = localStorage.getItem('accessToken');
-    if (token) {
-      api.defaults.headers.common['Authorization'] = token;
-    } else {
-      delete api.defaults.headers.common['Authorization'];
-    }
+    // const token = localStorage.getItem('accessToken');
+    // if (token) {
+    //   api.defaults.headers.common['Authorization'] = token;
+    // } else {
+    //   delete api.defaults.headers.common['Authorization'];
+    // }
     return api.post(url, data);
   };
   
